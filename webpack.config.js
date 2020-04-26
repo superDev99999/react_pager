f26cb48b0d4eec4592705842dad98321c356345c
superDev99999 <fencer.dev.9203@gmail.com>
2020-04-26 12:42:32 -0400

    switched webpack mode to development

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const webpack = require('webpack');
 const path = require('path');
 
 const config = {
-  mode: 'production',
+  mode: 'development',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
